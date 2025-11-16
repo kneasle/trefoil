04a7b1840a9f2abb1fbc4a60fece862a633558e0
Kneasle <kneasle@gmail.com>
2025-11-16 21:53:56 +0000

    Copy a bunch of code from polymod and get it compiling

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+mod viewport;
+
+use three_d::{egui::Color32, *};
+
+const COLOR_THEME: catppuccin_egui::Theme = catppuccin_egui::MOCHA;
+
+fn main() {
+    // Create window
+    let window = Window::new(WindowSettings {
+        title: "Trefoil".to_string(),
+        ..Default::default()
+    })
+    .unwrap();
+    let context = window.gl();
+
+    // Create model view
+    let mut view = viewport::Viewport::new(&context, window.viewport());
+
+    // Main loop
+    let mut gui = three_d::GUI::new(&context);
+    window.render_loop(move |mut frame_input| {
+        // Render GUI
+        let mut left_panel_width = 0.0;
+        let mut right_panel_width = 0.0;
+        let mut redraw = gui.update(
+            &mut frame_input.events,
+            frame_input.accumulated_time,
+            frame_input.viewport,
+            frame_input.device_pixel_ratio,
+            |egui_context| {
+                use three_d::egui::*;
+                // Set colors
+                catppuccin_egui::set_theme(egui_context, COLOR_THEME);
+                // Left panel
+                let response =
+                    SidePanel::left("left-panel")
+                        .min_width(300.0)
+                        .show(egui_context, |ui| {
+                            ui.heading("Models");
+                            ui.separator();
+                        });
+                left_panel_width = response.response.rect.width();
+                // Right panel
+                let response =
+                    SidePanel::right("right-panel")
+                        .min_width(250.0)
+                        .show(egui_context, |ui| {
+                            ui.heading("View");
+                        });
+                right_panel_width = response.response.rect.width();
+            },
+        );
+
+        // Calculate remaining viewport
+        let wl = (left_panel_width * frame_input.device_pixel_ratio) as i32;
+        let wr = (right_panel_width * frame_input.device_pixel_ratio) as i32;
+        let width = frame_input.viewport.width as i32 - wl - wr;
+        let viewport = Viewport {
+            x: wl,
+            y: 0,
+            width: width.max(1) as u32,
+            height: frame_input.viewport.height,
+        };
+
+        // Update the 3D view
+        redraw |= view.update(&mut frame_input, viewport);
+        if redraw {
+            let screen = frame_input.screen();
+            screen.clear(clear_state_for_egui_color(COLOR_THEME.base));
+            view.render(&screen);
+            screen.write(|| gui.render()).unwrap();
+        }
+
+        FrameOutput {
+            swap_buffers: redraw,
+            wait_next_event: !redraw,
+            ..Default::default()
+        }
+    });
+}
+
+fn clear_state_for_egui_color(clear_color: Color32) -> three_d::ClearState {
+    let clear_state = three_d::ClearState::color_and_depth(
+        clear_color.r() as f32 / 255.0,
+        clear_color.g() as f32 / 255.0,
+        clear_color.b() as f32 / 255.0,
+        1.0,
+        1.0,
+    );
+    clear_state
+}
---
+++ src/viewport.rs
@@ -0,0 +1,103 @@
+use std::ops::Deref;
+
+use three_d::*;
+
+/// The 3D viewport used to display a model
+pub(crate) struct Viewport {
+    context: Context,
+
+    camera: Camera,
+    control: OrbitControl,
+
+    face_material: PhysicalMaterial,
+    wireframe_material: PhysicalMaterial,
+}
+
+impl Viewport {
+    pub fn new(context: &Context, viewport: three_d::Viewport) -> Self {
+        // Camera
+        let target = vec3(0.0f32, 0.0, 0.0);
+        let scene_radius = 6.0f32;
+        let camera = Camera::new_perspective(
+            viewport,
+            target + scene_radius * vec3(0.0, 0.0, 1.0),
+            target,
+            vec3(0.0, 1.0, 0.0),
+            degrees(45.0),
+            0.1,
+            1000.0,
+        );
+        let control = OrbitControl::new(camera.target(), 0.1 * scene_radius, 100.0 * scene_radius);
+
+        // Materials
+        let mut face_material = PhysicalMaterial::new_opaque(
+            context,
+            &CpuMaterial {
+                albedo: Srgba::WHITE,
+                roughness: 0.7,
+                metallic: 0.0,
+                ..Default::default()
+            },
+        );
+        face_material.render_states.cull = Cull::Back;
+        let mut wireframe_material = PhysicalMaterial::new_opaque(
+            context,
+            &CpuMaterial {
+                albedo: Srgba::WHITE,
+                roughness: 0.7,
+                metallic: 0.0,
+                ..Default::default()
+            },
+        );
+        wireframe_material.render_states.cull = Cull::Back;
+
+        Self {
+            context: context.clone(),
+
+            camera,
+            control,
+
+            face_material,
+            wireframe_material,
+        }
+    }
+
+    pub fn update(&mut self, frame_input: &mut FrameInput, viewport: three_d::Viewport) -> bool {
+        let mut redraw = frame_input.first_frame;
+        redraw |= self.camera.set_viewport(viewport);
+        redraw |= self
+            .control
+            .handle_events(&mut self.camera, &mut frame_input.events);
+        redraw
+    }
+
+    pub fn render(&mut self, target: &RenderTarget) {
+        // Lights
+        let ambient = AmbientLight::new(&self.context, 0.7, Srgba::WHITE);
+        let directional0 =
+            DirectionalLight::new(&self.context, 2.0, Srgba::WHITE, vec3(-1.0, -1.0, -1.0));
+        let directional1 =
+            DirectionalLight::new(&self.context, 2.0, Srgba::WHITE, vec3(1.0, 1.0, 1.0));
+        let lights = [&ambient as &dyn Light, &directional0, &directional1];
+
+        // Meshes
+        // TODO: Add some caching to not send these to the GPU every frame
+        let meshes: Vec<Box<dyn Object>> = Vec::new();
+        /*
+        if let Some(cpu_mesh) = model.face_mesh() {
+            let mesh = Mesh::new(&self.context, &cpu_mesh);
+            meshes.push(Box::new(Gm::new(mesh, &self.face_material)));
+        }
+        meshes.push(Box::new(Gm::new(
+            model.edge_mesh(&self.context, edges_to_highlight),
+            &self.wireframe_material,
+        )));
+        meshes.push(Box::new(Gm::new(
+            model.vertex_mesh(&self.context),
+            &self.wireframe_material,
+        )));
+        */
+
+        target.render(&self.camera, meshes.iter().map(Deref::deref), &lights);
+    }
+}
